ed7f3787811238fae2206bda705f4b4dc183d19a
cywang <cywang.eda@gmail.com>
2021-01-29 04:36:37 +0800

    add package

--- install_conda_package.sh
+++ install_conda_package.sh
@@ -4,4 +4,4 @@ do
 done
 conda config --set channel_priority false
 conda update -n base -c defaults conda
-conda install -y conda python=3.* numpy lxml jupyter matplotlib pandas ipython future Pillow cython networkx psutil requests xlrd scipy scikit-learn parse jupyterlab=3.0.* jupyterlab-dash openssl electron jupytext plotly plotly-orca yarn python-pptx autopep8 snakeviz fs.sshfs openpyxl=3.0.0 keras imageai opencv sqlite postgresql psycopg2 aiohttp pgspecial aiohttp ccache seaborn jsonlines conda-build conda-verify sympy
+conda install -y conda python=3.* numpy lxml matplotlib pandas ipython future Pillow cython networkx psutil requests xlrd scipy scikit-learn parse jupyterlab=3.0.* jupyterlab-dash openssl electron jupytext plotly plotly-orca yarn python-pptx autopep8 snakeviz fs.sshfs openpyxl=3.0.0 keras imageai opencv sqlite postgresql psycopg2 aiohttp pgspecial aiohttp ccache seaborn jsonlines sympy
