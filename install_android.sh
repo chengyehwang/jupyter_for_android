3d258c9ff5288e36865605fb88e041af42854bae
cywang <cywang.eda@gmail.com>
2021-01-29 05:03:49 +0800

    add sourceme

--- install_android.sh
+++ install_android.sh
@@ -1,1 +1,7 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install autoconf automake libtool openssl pkg-config
+brew tap homebrew/cask-versions
+brew cask install homebrew/cask-versions/adoptopenjdk8
+
 pip install python-for-android
